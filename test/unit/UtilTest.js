89b5f20390ffb7eb80090d6179deb700b63950c5
nassim.kirouane <nassim.kirouane@inovia-team.com>
2018-01-14 20:43:06 +0100

    add code

--- test/unit/UtilTest.js
+++ test/unit/UtilTest.js
@@ -1,4 +1,4 @@
-describe("Util", function () {
+describe("Util - Factorial", function () {
     [[1, 1], [2, 2], [3, 6], [4, 24]].forEach(function (data) {
         it("should compute factorial of " + data[0] + " and find " + +data[1], function () {
             var result = Util.factorial(data[0]);
